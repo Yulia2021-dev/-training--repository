63ea81c607f9eb16278a7c7cd47f326e81c92077
Yulia <ulaboyko@ukr.net>
2021-12-09 11:35:06 +0200

    Rewrites work from hooks to state

--- src/App.js
+++ src/App.js
@@ -1,45 +1,51 @@
-import { useState } from "react";
+import React, { Component } from "react";
 import FeedbackOptions from "./components/feedbackOptions/FeedbackOptions.jsx";
 import Statistics from "./components/statistics/Statistics.jsx";
 
 import "./App.css";
 
-const initialRating = 0;
-
-export default function App() {
-  const [good, setGood] = useState(initialRating);
-  const [neutral, setNeutral] = useState(initialRating);
-  const [bad, setBad] = useState(initialRating);
-
-  const incrementOption = (option) => {
-    if (option === "good") {
-      setGood(good + 1);
-    } else if (option === "neutral") {
-      setNeutral(neutral + 1);
-    } else if (option === "bad") {
-      setBad(bad + 1);
-    } else {
-      console.error("No matched option");
-    }
-  };
-
-  const total = good + neutral + bad;
-  let expr = good / total;
-  if (isNaN(expr)) {
-    expr = 0;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+    this.incrementOption = this.incrementOption.bind(this);
   }
-  const positivePercentage = Math.floor(expr * 100);
-  const options = ["good", "neutral", "bad"];
-  return (
-    <div className="App">
-      <FeedbackOptions options={options} onLeaveFeedback={incrementOption} />
-      <Statistics
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        total={total}
-        positivePercentage={positivePercentage}
-      />
-    </div>
-  );
+
+  incrementOption(option) {
+    this.setState((state) => {
+      return {
+        [option]: state[option] + 1,
+      };
+    });
+  }
+
+  render() {
+    const total = Object.values(this.state).reduce((a, b) => a + b);
+    let expr = this.state.good / total;
+    if (isNaN(expr)) {
+      expr = 0;
+    }
+    const positivePercentage = Math.floor(expr * 100);
+    return (
+      <div className="App">
+        <FeedbackOptions
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.incrementOption}
+        />
+        <Statistics
+          good={this.state.good}
+          neutral={this.state.neutral}
+          bad={this.state.bad}
+          total={total}
+          positivePercentage={positivePercentage}
+        />
+      </div>
+    );
+  }
 }
+
+export default App;
